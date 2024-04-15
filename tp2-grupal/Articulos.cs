e3d156039c712181d970bb5d0c01270ad1dbcee4
Macarena-Casas <macarenacasas.mc@gmail.com>
2024-04-15 00:45:46 -0300

    agregue la clase articulos con sus metodos-sin funcionalidad por el momento

---
+++ tp2-grupal/Articulos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2_grupal
+{
+    class Articulos
+    {
+        public string nombre_artticulo {get; set; }
+        public int codigo_articulo { get; set; }
+        public float precio_articulo { get; set; }
+        public string descripcion_articulo { get; set; }
+        //public Categoria categoria_articulo { get; set; }
+        // public Marca marca_articulo { get; set; }
+
+        public void Detalles_articulo()
+        {
+
+        }
+        public void Agregar_articulo()
+        {
+
+        }
+        public void Modificar_articulo()
+        {
+
+        }
+        public void Eliminar_articulo()
+        {
+
+        }
+        public void Listar_articulos()
+        {
+
+        }
+        public void Busqueda_articulos()
+        {
+
+        }
+}
+}
