78a96c2da26745d00c67e3f87d196e3bef4deeb0
AlejandroBeron <ale.frecuenci@gmail.com>
2024-04-15 01:05:23 -0300

    Agrego la clase categoria y algunas funciones sin su funcionamiento para ir avanzando

---
+++ tp2-grupal/Categoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2_grupal
+{
+    class Categoria
+    {
+      
+        public string nombre { get; set; }
+        public int codigo_categoria { get; set; }
+        public string descripcion_categoria { get; set; }
+
+        public void ListarCategorias()
+        {
+
+        }
+        public void AgregarCategorias()
+        {
+
+        }
+        public void EliminarCategorias()
+        {
+
+        }
+
+    }
+}
