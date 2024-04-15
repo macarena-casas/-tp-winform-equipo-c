f7e8d9060d295645d6065c06000623d24fdd868c
martincasas747 <martinca747@gmail.com>
2024-04-15 16:20:16 -0300

    clase marca

---
+++ tp2-grupal/marcas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp2_grupal
+{
+    internal class marcas
+    {
+     public string nombre_marca {  get; set; }
+     public int codigo_marca { get; set; }
+     
+
+
+        public void listar_marcas()
+        {
+
+        }
+        public void agregar_marca()
+        {
+
+        }
+        public void eliminar_marca()
+        {
+
+        }
+    }
+}
